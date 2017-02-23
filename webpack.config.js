4aa0f4e80e7edc9ee8f17f97485801094f2b2dd3
webxzy <webxzy@qq.com>
2017-02-24 01:48:32 +0800

    添加hash

--- webpack.config.js
+++ webpack.config.js
@@ -7,66 +7,66 @@ var ROOT_PATH = path.resolve(__dirname);
 var APP_PATH = path.resolve(ROOT_PATH, 'app');
 var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
 
+// 使用了这个css模块，就不能支持热加载了
 var extractCSS = new ExtractTextPlugin('stylesheets/[name].css');
 
-
 module.exports = {
-	entry: {
-		app: path.resolve(APP_PATH, 'app.jsx')
-	},
-	output: {
-		path: BUILD_PATH,
-		filename: 'bundle.js'
-	},
-	devtool: 'eval',
-	devServer: {
-		historyApiFallback: true,
-		hot: true,
-		inline: true,
-		progress: true
-	},
-	resolve: {
-		extensions: ['', '.js', '.jsx'],
-		root: APP_PATH
-	},
-	module: {
-		preLoaders: [{
-			test: /\.jsx?$/,
-			loaders: ['eslint'],
-			include: APP_PATH
-		}],
-		loaders: [{
-			test: /\.jsx?$/,
-			loaders: ['babel'],
-			include: APP_PATH
-		}, {
-			test: /\.scss$/,
-			loader: extractCSS.extract(['css', 'sass'])
-			// loaders: ['style', 'css', 'sass']
-		}, {
-	        test:/\.(js|jsx)$/,
-	        loader:'babel',
-	        include:path.join(__dirname, 'src'),
-	        query: {
-	          plugins: [
-	            ["react-transform", {
-	              transforms: [{
-	                transform:"react-transform-hmr",
-	                imports: ["react"],
-	                locals: ["module"]
-	              }, {
-	                "transform":"react-transform-catch-errors",
-	                "imports": ["react", "redbox-react"]
-	              }]
-	            }]
-	          ]
-	        }
+    entry: {
+        app: path.resolve(APP_PATH, 'app.jsx')
+    },
+    output: {
+        path: BUILD_PATH,
+        filename: 'bundle.js'
+    },
+    devtool: 'eval',
+    devServer: {
+        historyApiFallback: true,
+        hot: true,
+        inline: true,
+        progress: true
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx'],
+        root: APP_PATH
+    },
+    module: {
+        preLoaders: [{
+            test: /\.jsx?$/,
+            loaders: ['eslint'],
+            include: APP_PATH
+        }],
+        loaders: [{
+            test: /\.jsx?$/,
+            loaders: ['babel'],
+            include: APP_PATH
+        }, {
+            test: /\.scss$/,
+            loader: extractCSS.extract(['css', 'sass']) // loaders: ['style', 'css', 'sass']
+        }, {
+            test: /\.(js|jsx)$/,
+            loader: 'babel',
+            include: path.join(__dirname, 'src'),
+            query: {
+                plugins: [
+                    ["react-transform", {
+                        transforms: [{
+                            transform: "react-transform-hmr",
+                            imports: ["react"],
+                            locals: ["module"]
+                        }, {
+                            "transform": "react-transform-catch-errors",
+                            "imports": ["react", "redbox-react"]
+                        }]
+                    }]
+                ]
+            }
         }]
-	},
-	plugins: [
-		extractCSS,
-		new HtmlWebPackPlugin({
-			title: 'Deskmark'
-		})
-	]
+    },
+    plugins: [
+        extractCSS,
+        new HtmlWebPackPlugin({
+            title: 'Deskmark',
+            hash: true
+        })
+    ]
 }
